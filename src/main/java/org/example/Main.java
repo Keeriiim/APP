5faa7b3b4a6e4c1205bb36740f2aa4f3a0708c73
Kerim <kerim_kozo@hotmail.com>
2023-10-10 22:33:46 +0200

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.example.dynamickafkaspring.Payload.UserJson;
+
+import java.net.MalformedURLException;
+
+public class Main {
+    public static void main(String[] args) throws MalformedURLException {
+        Menu menu = new Menu();
+
+        System.out.println("Project Done");
+
+
+    }
+}
